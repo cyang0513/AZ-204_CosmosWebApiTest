8c52d4938df52caf8020ffecf5c205158d956499
Chengkai Yang <yangchengkai@gmail.com>
2021-03-06 15:23:20 +0100

    swagger

--- CosmosWebApiTest/Startup.cs
+++ CosmosWebApiTest/Startup.cs
@@ -40,7 +40,11 @@ namespace CosmosWebApiTest
       {
          app.UseDeveloperExceptionPage();
          app.UseSwagger();
-         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CosmosWebApiTest v1"));
+         app.UseSwaggerUI(c =>
+         {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "CosmosWebApiTest v1");
+            c.RoutePrefix = string.Empty;
+         });
 
          app.UseHttpsRedirection();
 
